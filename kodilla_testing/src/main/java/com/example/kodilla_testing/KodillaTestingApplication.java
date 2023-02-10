ab6fcc8bdc48268578678f5f61b8cef6902686bf
Mac <mac@Mac-Kasi.local>
2023-02-10 18:32:55 +0100

    6.1 Added kodilla-testing module

---
+++ kodilla_testing/src/main/java/com/example/kodilla_testing/KodillaTestingApplication.java
@@ -0,0 +1,13 @@
+package com.example.kodilla_testing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodillaTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KodillaTestingApplication.class, args);
+	}
+
+}
