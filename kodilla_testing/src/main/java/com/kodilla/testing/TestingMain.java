17c65a34e5e865112a374b25f0df3aa8e1df6b11
Kasia Gierasimczuk <k.gierasimczuk@gmail.com>
2023-02-16 20:50:36 +0100

    6.2 Testing of Calculator

--- kodilla_testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla_testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,47 @@
 package com.kodilla.testing;
 
+//import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
+
+
 public class TestingMain {
    public static void main(String[] args) {
-      System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+      //    System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+
+//      SimpleUser simpleUser = new SimpleUser("theForumUser");
+//
+//      String result = simpleUser.getUsername();
+//
+//      if (result.equals("theForumUser")) {
+//         System.out.println("test OK");
+//      } else {
+//         System.out.println("Error!");
+//      }
+//
+//      // tu umieść kolejny test ->
+//      System.out.println("Test - pierwszy test jednostkowy:");
+
+      //Calculator app test
+      Calculator calculator = new Calculator();
+
+      int a = 8;
+      int b = 3;
+
+      int addingResult = calculator.add(a, b);
+
+      if (addingResult == 11) {
+         System.out.println("Adding test OK");
+      } else {
+         System.out.println("Error in adding test!");
+      }
+
+      int subtractingResult = calculator.subtract(a, b);
+
+      if (subtractingResult == 5) {
+         System.out.println("Subtracting test OK");
+      } else {
+         System.out.println("Error in subtracting test!");
+      }
+
    }
 }
---
+++ kodilla_testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,23 @@
+package com.kodilla.testing.calculator;
+
+public class Calculator {
+
+   //  W tym pakiecie stwórz i zaimplementuj nową klasę Calculator, reprezentującą prosty kalkulator z dwoma funkcjami:
+   //  add(int a, int b) oraz subtract(int a, int b). W metodzie main klasy TestingMain napisz test sprawdzający
+   //  działanie klasy Calculator (po jednym teście dla każdej metody kalkulatora).
+//   private int a;
+//   private int b;
+//
+//   public Calculator(int a, int b) {
+//      this.a = a;
+//      this.b = b;
+//   }
+
+   public int add(int a, int b) {
+      return a + b;
+   }
+
+   public int subtract(int a, int b) {
+      return a - b;
+   }
+}
