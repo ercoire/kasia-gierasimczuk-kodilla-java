d9de851988d9d415129ee1396d1cbd290e014e68
Kasia Gierasimczuk <k.gierasimczuk@gmail.com>
2023-04-21 13:37:26 +0200

    8.4 IndexOutOfBoundsException example

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/index/VideoCollector.java
@@ -0,0 +1,15 @@
+package com.kodilla.exception.index;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class VideoCollector {
+   public List<String> getCollection() {
+      ArrayList<String> videos = new ArrayList<>();
+      videos.add("Extra Movie");
+      return videos;
+
+
+   }
+
+}
