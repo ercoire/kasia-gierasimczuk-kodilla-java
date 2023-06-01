c818f71a1c92354d8b468f1a5b331b0fa43abbc8
Kasia Gierasimczuk <k.gierasimczuk@gmail.com>
2023-06-01 16:33:43 +0200

    Zadanie 14.4 - Spring Web

---
+++ kodilla-spring-web/src/main/java/com/example/kodillaspringweb/KodillaSpringWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.kodillaspringweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodillaSpringWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KodillaSpringWebApplication.class, args);
+	}
+
+}
---
+++ kodilla-spring-web/src/test/java/com/example/kodillaspringweb/KodillaSpringWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kodillaspringweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KodillaSpringWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
