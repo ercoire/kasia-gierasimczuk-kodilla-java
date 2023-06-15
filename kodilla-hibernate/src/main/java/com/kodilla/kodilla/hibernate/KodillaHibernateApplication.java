05b0198a23101e632f0ef7400dff47815280ac51
Kasia Gierasimczuk <k.gierasimczuk@gmail.com>
2023-06-15 17:51:24 +0200

    Zadanie 17.1 - Dołączamy framework Hibernate.

---
+++ kodilla-hibernate/src/main/java/com/kodilla/kodilla/hibernate/KodillaHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla.kodilla.hibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodillaHibernateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KodillaHibernateApplication.class, args);
+    }
+
+}
---
+++ kodilla-hibernate/src/test/java/com/kodilla/kodilla/hibernate/KodillaHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodilla.kodilla.hibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KodillaHibernateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
